9b53198f567e1fba75a608c16102bdf4130c0c97
James Vickery <dev@jamesvickery.net>
2018-05-28 10:30:44 +0100

    Install oh-my-zsh and Vundle

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+# Make vim swap directory
+mkdir -p $HOME/.vim/swap/
+
+# Clone Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true
+
 # Install dotfiles to HOME
-
 dotfiles=(".vimrc" ".zshrc" ".gitconfig" ".profile")
-
 # Copy into dotfiles repo
 for dotfile in "${dotfiles[@]}";do
     cp "./${dotfile}" $HOME/
 done
 
-mkdir -p $HOME/.vim/swap/
+# Install Vundle plugins
+vim +PluginInstall +qall
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Add jmsv theme to zsh themes directory
 cp jmsv.zsh-theme ~/.oh-my-zsh/themes/
 
