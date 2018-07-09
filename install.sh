b907de49cb23e6696efadd70a1a7a090a2245591
James Vickery <dev@jamesvickery.net>
2018-07-09 22:10:59 +0100

    Added fonts

--- install.sh
+++ install.sh
@@ -19,5 +19,9 @@ vim +PluginInstall +qall
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # Add jmsv theme to zsh themes directory
+
 cp jmsv.zsh-theme ~/.oh-my-zsh/themes/
+
+mkdir -p ~/.fonts
+mv fonts/* ~/.fonts/
 
