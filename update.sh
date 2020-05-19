c4d4a29a26daa79dd2710eabea6a365277a6b884
James Vickery <dev@jamesvickery.net>
2020-05-19 13:27:50 +0100

    fix update script

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+set -x
+
 # Update dotfiles from HOME
-dotfiles=(".vimrc" ".zshrc" ".gitconfig" ".profile", ".nvmlazy", ".aliases")
+dotfiles=(".vimrc" ".zshrc" ".gitconfig" ".profile" ".aliases")
 for dotfile in "${dotfiles[@]}";do
-    cp "${HOME}/${dotfile}" .
+    cp ${HOME}/${dotfile} .
 done
 
 # Update jmsv theme
-cp ~/.oh-my-zsh/themes/jmsv.zsh-theme . 
+cp ~/.oh-my-zsh/themes/jmsv.zsh-theme .
+
