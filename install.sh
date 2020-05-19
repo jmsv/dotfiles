1b6bd3c5dc7240de9200f79eed0d43d66d7c651d
James Vickery <dev@jamesvickery.net>
2020-05-19 13:19:58 +0100

    dotfiles list fix & reorder

--- install.sh
+++ install.sh
@@ -6,8 +6,12 @@ mkdir -p $HOME/.vim/swap/
 # Clone Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -f ~/.zshrc.pre-oh-my-zsh
+
 # Install dotfiles to HOME
-dotfiles=(".vimrc" ".zshrc" ".gitconfig" ".profile", ".nvmlazy", ".aliases")
+dotfiles=(".vimrc" ".zshrc" ".gitconfig" ".profile" ".nvmlazy" ".aliases")
 for dotfile in "${dotfiles[@]}";do
     cp "./${dotfile}" $HOME/
 done
@@ -18,9 +22,6 @@ touch $HOME/.env
 # Install Vundle plugins
 vim +PluginInstall +qall
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # oh-my-zsh zsh-autosuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
@@ -30,5 +31,6 @@ cp jmsv.zsh-theme ~/.oh-my-zsh/themes/
 # Install lazydocker
 # curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
+# Fonts on Ubuntu
 mkdir -p ~/.fonts
 cp fonts/* ~/.fonts/
