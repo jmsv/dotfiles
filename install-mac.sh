3ad9d44289380048a23958b47b64b071e8b639e1
James Vickery <dev@jmsv.me>
2022-08-17 01:44:30 +0100

    install script for mac

---
+++ install-mac.sh
@@ -0,0 +1,29 @@
+# --- Xcode CLI tools
+xcode-select --install
+
+# --- Homebrew CLI
+
+# https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# --- Stuff from Homebrew
+
+brew update
+
+# https://github.com/Ji4n1ng/OpenInTerminal
+brew install --cask openinterminal
+
+# https://github.com/exelban/stats
+brew install --cask stats
+
+# https://github.com/AlDanial/cloc
+brew install cloc
+
+# https://imagemagick.org/script/download.php
+brew install imagemagick
+
+# https://httpie.io/docs/cli/macos
+brew install httpie
+
+# https://github.com/julienXX/terminal-notifier
+brew install terminal-notifier
